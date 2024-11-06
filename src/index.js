81b49233253a11d28a3805aa64f40dffd1fc4754
Jonas <jonasbal1999@gmail.com>
2024-11-06 22:14:12 +0200

    CookieYes styles

--- src/index.js
+++ src/index.js
@@ -1130,6 +1130,34 @@ function setCaseUrl() {
 	}
 }
 
+function cookiesPopup() {
+	function applyCustomCode() {
+		const preferenceCenter = document.querySelector(".cky-preference-center");
+		if (!preferenceCenter) return;
+
+		preferenceCenter.setAttribute("data-lenis-prevent", "");
+		const closeImg = preferenceCenter.querySelector(".cky-btn-close img");
+		const newCloseImage = "https://cdn.prod.website-files.com/66deddeed7fdcd89b6b4d6a7/672bc1e32815078091d25109_close.svg";
+		closeImg.src = newCloseImage;
+	}
+
+	// Mutation observer to wait for cookieYes banner to load
+	const observer = new MutationObserver((mutationsList, observer) => {
+		for (const mutation of mutationsList) {
+			if (mutation.type === "childList") {
+				// Check if the cookie banner is now in the DOM
+				if (document.querySelector(".cky-consent-container")) {
+					applyCustomCode();
+					observer.disconnect(); // Stop observing once the banner is found
+					break;
+				}
+			}
+		}
+	});
+
+	observer.observe(document.body, { childList: true, subtree: true });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	gsap.registerPlugin(ScrollTrigger);
 	lenisSmoothScroll();
@@ -1157,4 +1185,5 @@ document.addEventListener("DOMContentLoaded", () => {
 	formURLfield();
 	cmsPagination();
 	setCaseUrl();
+	cookiesPopup();
 });
